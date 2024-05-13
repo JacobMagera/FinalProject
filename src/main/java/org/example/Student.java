1f6360d7365ed626f2feddfc79ecad7892426bb9
6260999 <6260999@edu.vaniercollege.qc.ca>
2024-05-13 15:15:59 -0400

    Added Student.java class, added the fields but have yet to add the methods.

---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Student {
+    private String studentId;
+    private String studentName;
+    private Gender gender;
+    private Department department;
+    private ArrayList<Course> registeredCourses;
+    private static int nextId = 1;
+}
