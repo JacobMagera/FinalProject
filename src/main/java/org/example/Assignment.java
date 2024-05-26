bd91660c146ce03e80586851ee8aa8f62e7c8b72
Jacob <6260999@edu.vaniercollege.qc.ca>
2024-05-26 04:33:15 -0400

    Added methods for the Assignment class

--- src/main/java/org/example/Assignment.java
+++ src/main/java/org/example/Assignment.java
@@ -3,6 +3,7 @@ package org.example;
 import lombok.ToString;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 @ToString
 
@@ -14,4 +15,36 @@ public class Assignment {
     private double assignmentAverage;
     private ArrayList<Integer> scores;
     private static int nextId = 1;
+
+    public void calcAssignmentAvg() {
+        int sum = 0;
+        int count = 0;
+        for (Integer score : scores) {
+            if (score != null) {
+                sum += score;
+                count++;
+            }
+        }
+        this.assignmentAverage = count == 0 ? 0 : (double) sum / count;
+    }
+
+    public void generateRandomScore() {
+        Random rand = new Random();
+        for (int i = 0; i < scores.size(); i++) {
+            int rangeSelector = rand.nextInt(11); // generates a number between 0 and 10
+            int score;
+            if (rangeSelector == 0) {
+                score = rand.nextInt(60); // 0 to 59
+            } else if (rangeSelector == 1 || rangeSelector == 2) {
+                score = 60 + rand.nextInt(10); // 60 to 69
+            } else if (rangeSelector == 3 || rangeSelector == 4) {
+                score = 70 + rand.nextInt(10); // 70 to 79
+            } else if (rangeSelector <= 8) {
+                score = 80 + rand.nextInt(10); // 80 to 89
+            } else {
+                score = 90 + rand.nextInt(11); // 90 to 100
+            }
+            scores.set(i, score);
+        }
+    }
 }
